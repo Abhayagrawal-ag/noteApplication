5d5e532f3f7915aa28b5478d06b63a66747fdb8f
Abhayagrawal-ag <your.email@example.com>
2025-05-28 16:47:33 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   server: {
-    // Enables SPA fallback so that refreshing doesn't give 404
-    historyApiFallback: true
-  }
+   
 });
 // vite.config.js
